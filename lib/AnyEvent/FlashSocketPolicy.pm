197912427a6d8b6bbae478b8fb3acab1c5bf9441
keroyonn <keroyonn@gmail.com>
2010-09-13 22:45:51 +0900

    initial commit

---
+++ lib/AnyEvent/FlashSocketPolicy.pm
@@ -0,0 +1,32 @@
+package AnyEvent::FlashSocketPolicy;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+AnyEvent::FlashSocketPolicy -
+
+=head1 SYNOPSIS
+
+  use AnyEvent::FlashSocketPolicy;
+
+=head1 DESCRIPTION
+
+AnyEvent::FlashSocketPolicy is
+
+=head1 AUTHOR
+
+keroyonn E<lt>keroyonn@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
